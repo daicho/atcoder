fe179f704642c79927028fc726e1f4e8c6deaf74
daicho <daichimax78@gmail.com>
2020-02-12 09:33:48 +0900

    テンプレートのrepマクロの引数を可変に

--- dp/b.cpp
+++ dp/b.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
 
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#define select(_1, _2, x, ...) x
+
+#define repn(i, n) for (int i = 0; i < (n); i++)
 #define repr(i, a, b) for (int i = (a); i < (b); i++)
-#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define rep(i, ...) select(__VA_ARGS__, repr, repn)(i, __VA_ARGS__)
+
+#define rrepn(i, n) for (int i = (n - 1); i >= 0; i--)
 #define rrepr(i, a, b) for (int i = (b - 1); i >= (a); i--)
+#define rrep(i, ...) select(__VA_ARGS__, rrepr, rrepn)(i, __VA_ARGS__)
 
 #define all(v) (v).begin(), (v).end();
 #define debug(x) cerr << #x << " = " << x << endl
@@ -20,7 +25,7 @@ int main() {
 
     vector<int> dp(n);
 
-    repr(i, 1, n) {
+    rep(i, 1, n) {
         dp[i] = dp[i - 1] + abs(h[i] - h[i - 1]);
         rrepr(j, i - k, i - 1) {
             if (j < 0) break;
--- dp/d.cpp
+++ dp/d.cpp
@@ -1,14 +1,9 @@
 #include <bits/stdc++.h>
 
-#define select(_1, _2, x, ...) x
-
-#define repn(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, n) for (int i = 0; i < (n); i++)
 #define repr(i, a, b) for (int i = (a); i < (b); i++)
-#define rep(i, ...) select(__VA_ARGS__, repr, repn)(i, __VA_ARGS__)
-
-#define rrepn(i, n) for (int i = (n - 1); i >= 0; i--)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
 #define rrepr(i, a, b) for (int i = (b - 1); i >= (a); i--)
-#define rrep(i, ...) select(__VA_ARGS__, rrepr, rrepn)(i, __VA_ARGS__)
 
 #define all(v) (v).begin(), (v).end();
 #define debug(x) cerr << #x << " = " << x << endl
@@ -17,6 +12,14 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    int N, W;
+    cin >> N >> W;
+
+    vector<ll> w(N), v(N);
+    rep(i, N) cin >> w[i] >> v[i];
+
+    vector<vector<ll>> dp(N, vector<ll>(W));
+
 
     return 0;
 }
