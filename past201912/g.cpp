935ecbac6222af785a05ebd660344f7fde520e52
daicho <daichimax78@gmail.com>
2020-03-26 00:23:36 +0900

    第1回PASTのH問題まで実施

--- past201912/g.cpp
+++ past201912/g.cpp
@@ -10,6 +10,9 @@
 #define rrepr(i, a, b) for (int i = (b - 1); i >= (a); i--)
 #define rrep(i, ...) select(__VA_ARGS__, rrepr, rrepn)(i, __VA_ARGS__)
 
+#define umax(m, x) if (x > m) m = x;
+#define umin(m, x) if (x < m) m = x;
+
 #define all(v) v.begin(), v.end()
 #define debug(x) cerr << #x << " = " << x << endl
 
@@ -24,7 +27,41 @@ int main() {
     int n;
     cin >> n;
 
-    
+    vector<vector<ll>> a(n - 1, vector<ll>(n));
+
+    rep(i, n - 1) {
+        rep(j, i + 1, n) {
+            cin >> a[i][j];
+        }
+    }
+
+    vector<int> d(n, 0);
+    ll max = -LINF;
+
+    while (true) {
+        ll sum = 0;
+        rep(i, n - 1) {
+            rep(j, i + 1, n) {
+                if (d[i] == d[j])
+                    sum += a[i][j];
+            }
+        }
+
+        umax(max, sum);
+
+        d[0]++;
+        rep(i, n - 1) {
+            if (d[i] >= 3) {
+                d[i] = 0;
+                d[i + 1]++;
+            }
+        }
+
+        if (d[n - 1] >= 3)
+            break;
+    }
+
+    cout << max << endl;
 
     return 0;
 }
--- past201912/h.cpp
+++ past201912/h.cpp
@@ -10,6 +10,9 @@
 #define rrepr(i, a, b) for (int i = (b - 1); i >= (a); i--)
 #define rrep(i, ...) select(__VA_ARGS__, rrepr, rrepn)(i, __VA_ARGS__)
 
+#define umax(m, x) if (x > m) m = x;
+#define umin(m, x) if (x < m) m = x;
+
 #define all(v) v.begin(), v.end()
 #define debug(x) cerr << #x << " = " << x << endl
 
@@ -21,6 +24,70 @@ const ll LINF = 1e18;
 
 
 int main() {
+    ll n;
+    cin >> n;
+
+    vector<ll> c(n);
+    rep(i, n) cin >> c[i];
+
+    ll q;
+    cin >> q;
+
+    ll sum = 0;
+    ll sub_kisu = 0;
+    ll sub_all = 0;
+
+    ll min_kisu = LINF;
+    ll min_all = LINF;
+
+    for (int i = 0; i < n; i += 2)
+        umin(min_kisu, c[i]);
+
+    rep(i, n)
+        umin(min_all, c[i]);
+
+    rep(i, q) {
+        char t;
+        cin >> t;
+
+        if (t == '1') {
+            ll x, a;
+            cin >> x >> a;
+
+            if (c[x - 1] >= a + sub_all + ((x % 2) ? sub_kisu : 0)) {
+                c[x - 1] -= a;
+                sum += a;
+
+                umin(min_all, c[x - 1]);
+                if (x % 2)
+                    umin(min_kisu, c[x - 1]);
+            }
+        } else if (t == '2') {
+            ll a;
+            cin >> a;
+
+            if (min_kisu >= a) {
+                sub_kisu += a;
+                sum += a * (int)((n + 1) / 2);
+
+                min_kisu -= a;
+                umin(min_all, min_kisu);
+            }
+        } else if (t == '3') {
+            ll a;
+            cin >> a;
+
+            if (min_all >= a) {
+                sub_all += a;
+                sum += a * n;
+
+                min_all -= a;
+                min_kisu -= a;
+            }
+        }
+    }
+
+    cout << sum << endl;
 
     return 0;
 }
