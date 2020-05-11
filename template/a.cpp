f4ff0a34bdc74e4645674c918440f5803c3b0538
daicho <daichimax78@gmail.com>
2020-05-11 20:36:08 +0900

    テンプレートを更新

--- template/a.cpp
+++ template/a.cpp
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
 
-#define __sel(_1, _2, x, ...) x
+#define _sel(_1, _2, x, ...) x
 
 #define repn(i, n) for (ll i = 0; i < (n); i++)
-#define repr(i, a, b) for (ll i = (a); i < (b); i++)
-#define rep(i, ...) __sel(__VA_ARGS__, repr, repn)(i, __VA_ARGS__)
+#define reps(i, a, b) for (ll i = (a); i < (b); i++)
+#define rep(i, ...) _sel(__VA_ARGS__, reps, repn)(i, __VA_ARGS__)
 
-#define rrepn(i, n) for (ll i = (n - 1); i >= 0; i--)
-#define rrepr(i, a, b) for (ll i = (b - 1); i >= (a); i--)
-#define rrep(i, ...) __sel(__VA_ARGS__, rrepr, rrepn)(i, __VA_ARGS__)
+#define reppn(i, n) for (ll i = 1; i <= (n); i++)
+#define repps(i, a, b) for (ll i = (a); i <= (b); i++)
+#define repp(i, ...) _sel(__VA_ARGS__, repps, reppn)(i, __VA_ARGS__)
 
-#define umax(m, x) (m = max(m, x))
-#define umin(m, x) (m = min(m, x))
-#define rng(v) v.begin(), v.end()
-#define rrng(v) v.rbegin(), v.rend()
-#define bit(x, n) ((x) >> (n) & 1)
+#define rrepn(i, n) for (ll i = (n) - 1; i >= 0; i--)
+#define rreps(i, a, b) for (ll i = (b) - 1; i >= (a); i--)
+#define rrep(i, ...) _sel(__VA_ARGS__, rreps, rrepn)(i, __VA_ARGS__)
+
+#define rreppn(i, n) for (ll i = (n); i >= 1; i--)
+#define rrepps(i, a, b) for (ll i = (b); i >= (a); i--)
+#define rrepp(i, ...) _sel(__VA_ARGS__, rrepps, rreppn)(i, __VA_ARGS__)
+
+#define rng(v) (v).begin(), (v).end()
+#define rrng(v) (v).rbegin(), (v).rend()
 
 #define pb push_back
 #define mp make_pair
@@ -22,13 +27,10 @@
 #define fi first
 #define se second
 
-#define pt(x) cout << (x) << endl;
-#define ptv(v) for (auto __t1 : v) cout << __t1 << " "; cout << endl;
-#define ptl(v) for (auto __t1 : v) cout << __t1 << endl;
-#define shv(v) for (auto __t1 : v) cerr << __t1 << " "; cerr << endl;
-#define db(x) cerr << #x << ": " << (x) << endl;
-#define dbv(v) cerr << #v << ": "; shv(v);
-#define dbm(m) cerr << #m << ":" << endl; for (auto __t2 : m) { shv(__t2); }
+#define _shv(v) for (auto _t1 : v) cerr << _t1 << " "; cerr << endl;
+#define dbg(x) cerr << #x << ": " << (x) << endl;
+#define dbgv(v) cerr << #v << ": "; _shv(v);
+#define dbgm(m) cerr << #m << ":" << endl; for (auto _t2 : m) { _shv(_t2); }
 
 using namespace std;
 
@@ -39,8 +41,6 @@ using pll = pair<ll, ll>;
 
 using vi = vector<int>;
 using vl = vector<ll>;
-using vd = vector<double>;
-using vld = vector<ld>;
 using vb = vector<bool>;
 using vc = vector<char>;
 using vs = vector<string>;
@@ -49,8 +49,6 @@ using vpll = vector<pll>;
 
 using vvi = vector<vector<int>>;
 using vvl = vector<vector<ll>>;
-using vvd = vector<vector<double>>;
-using vvld = vector<vector<ld>>;
 using vvb = vector<vector<bool>>;
 using vvc = vector<vector<char>>;
 using vvs = vector<vector<string>>;
@@ -59,26 +57,27 @@ const int INF = 1e9;
 const ll LINF = 1e18;
 const ll MOD = 1e9 + 7;
 
-inline int gi() { int x; cin >> x; return x; }
-inline ll gl() { int x; cin >> x; return x; }
-inline char gc() { char x; cin >> x; return x; }
-inline string gs() { string x; cin >> x; return x; }
-inline int ni() { return gi() - 1; }
-inline ll nl() { return gl() - 1; }
+template<typename T, typename U>
+inline void getv(T& v, U n) { v = T(n); rep(i, n) cin >> v[i]; }
+template<typename T, typename U>
+inline void inv(T& v, U n) { v = T(n); rep(i, n) { cin >> v[i]; v[i]--; } }
+template<typename T, typename U>
+inline vvi getm(T& v, U m, U n) { v = T(n); rep(i, n) getv(v[i], m); return v; }
+template<typename T, typename U>
+inline vvi inm(T& v, U m, U n) { v = T(n); rep(i, n) inv(v[i], m); return v; }
 
-inline vi gvi(int n) { vi v; rep(i, n) v.pb(gi()); return v; }
-inline vl gvl(int n) { vl v; rep(i, n) v.pb(gl()); return v; }
-inline vc gvc(int n) { vc v; rep(i, n) v.pb(gc()); return v; }
-inline vs gvs(int n) { vs v; rep(i, n) v.pb(gs()); return v; }
-inline vi nvi(int n) { vi v; rep(i, n) v.pb(ni()); return v; }
-inline vl nvl(int n) { vl v; rep(i, n) v.pb(nl()); return v; }
+template<typename T> inline void prt(T x) { cout << (x) << endl; }
+template<typename T> inline void prtv(T v) { for (auto t : v) cout << t << " "; cout << endl; }
+template<typename T> inline void prtl(T v) { for (auto t : v) cout << t << endl; }
 
-inline vvi gvvi(int m, int n) { vvi v; rep(i, n) v.pb(gvi(m)); return v; }
-inline vvl gvvl(int m, int n) { vvl v; rep(i, n) v.pb(gvl(m)); return v; }
-inline vvc gvvc(int m, int n) { vvc v; rep(i, n) v.pb(gvc(m)); return v; }
-inline vvs gvvs(int m, int n) { vvs v; rep(i, n) v.pb(gvs(m)); return v; }
-inline vvi nvvi(int m, int n) { vvi v; rep(i, n) v.pb(nvi(m)); return v; }
-inline vvl nvvl(int m, int n) { vvl v; rep(i, n) v.pb(nvl(m)); return v; }
+template<typename T, typename U>
+inline bool cmax(T m, U x) { if (m < x) { m = x; return true; } return false; }
+template<typename T, typename U>
+inline bool cmin(T m, U x) { if (m > x) { m = x; return true; } return false; }
+
+template<typename T> inline int sz(T x) { return x.size(); }
+template<typename T> inline ll pow2(T n) { return 1ll << n; }
+template<typename T, typename U> inline int bit(T x, U n) { return x >> n & 1; }
 
 inline void yn(bool f) { cout << (f ? "Yes" : "No") << endl; }
 
