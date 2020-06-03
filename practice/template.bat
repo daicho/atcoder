735db8ea1c6c4de11e98b3e52642d1781922702a
daicho <daichimax78@gmail.com>
2020-06-03 11:28:08 +0900

    mintライブラリを追加

--- practice/template.bat
+++ practice/template.bat
@@ -3,5 +3,5 @@ set /P file_name="File Name : "
 set file_path=intermediate\%file_name%.cpp
 
 if not exist %file_path% (
-    copy Template.cpp %file_path% /-Y
+    copy template.cpp %file_path% /-Y
 )
