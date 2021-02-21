c010dad283d9aba196b0bbfde650b5955f8d6103
daicho <daichimax78@gmail.com>
2021-02-21 22:47:54 +0900

    arc113

---
+++ arc/arc113/b.cpp
@@ -0,0 +1,150 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using ll = long long int;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vc = vector<char>;
+using vs = vector<string>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vvb = vector<vector<bool>>;
+using vvc = vector<vector<char>>;
+using vvs = vector<vector<string>>;
+template<typename T> using vv = vector<vector<T>>;
+template<typename T> using vvv = vector<vector<vector<T>>>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ 
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int DX[4] = {1, 0, -1, 0};
+const int DY[4] = {0, 1, 0, -1};
+ 
+#define _sel(_1, _2, x, ...) x
+#define repn(i, n) for (ll i = 0; i < (n); i++)
+#define reps(i, a, b) for (ll i = (a); i < (b); i++)
+#define rep(i, ...) _sel(__VA_ARGS__, reps, repn)(i, __VA_ARGS__)
+#define reppn(i, n) for (ll i = 1; i <= (n); i++)
+#define repps(i, a, b) for (ll i = (a); i <= (b); i++)
+#define repp(i, ...) _sel(__VA_ARGS__, repps, reppn)(i, __VA_ARGS__)
+#define rrepn(i, n) for (ll i = (n) - 1; i >= 0; i--)
+#define rreps(i, a, b) for (ll i = (b) - 1; i >= (a); i--)
+#define rrep(i, ...) _sel(__VA_ARGS__, rreps, rrepn)(i, __VA_ARGS__)
+#define rreppn(i, n) for (ll i = (n); i >= 1; i--)
+#define rrepps(i, a, b) for (ll i = (b); i >= (a); i--)
+#define rrepp(i, ...) _sel(__VA_ARGS__, rrepps, rreppn)(i, __VA_ARGS__)
+ 
+#define pb push_back
+#define fi first
+#define se second
+#define rng(v) (v).begin(), (v).end()
+#define rrng(v) (v).rbegin(), (v).rend()
+#define siz(x) int((x).size())
+#define pow2(x) (1ll << (x))
+#define bit(x, n) ((x) >> (n) & 1)
+#define prt(x) cout << (x) << endl
+#define dbg(x) dbgn(#x, x)
+#define dec() cout << fixed << setprecision(15)
+ 
+template<typename T, typename U> inline bool umax(T& m, U x) { if (m < x) { m = x; return true; } return false; }
+template<typename T, typename U> inline bool umin(T& m, U x) { if (m > x) { m = x; return true; } return false; }
+template<typename T> inline void errv(T& v) { for (auto x: v) cerr << x << " "; cerr << endl; }
+inline void errv(vb& v) { for (auto x: v) cerr << (x ? 1 : 0) << " "; cerr << endl; }
+template<typename T> inline void dbgn(string n, T x) { cerr << n << ": " << x << endl; }
+template<typename T> inline void dbgn(string n, vector<T>& v) { cerr << n << ": "; errv(v); }
+template<typename T> inline void dbgn(string n, vv<T>& m) { cerr << n << ":" << endl; for (auto& v: m) errv(v); }
+ 
+struct UnionFind {
+    vi p;
+    UnionFind(int n): p(n, -1) { }
+    int root(int x) { if (p[x] < 0) return x; else return p[x] = root(p[x]); }
+    int size(int x) { return -p[root(x)]; }
+    void unite(int x, int y) {
+        int rx = root(x), ry = root(y);
+        if (rx == ry) return; if (p[rx] > p[ry]) swap(rx, ry);
+        p[rx] += p[ry]; p[ry] = rx;
+    }
+};
+ 
+ll mod = 10;
+class mint {
+public:
+    ll x;
+    mint(ll x=0) : x((x%mod+mod)%mod) {}
+    mint operator-() const { 
+      return mint(-x);
+    }
+    mint& operator+=(const mint& a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint& a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const  mint& a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+    mint operator+(const mint& a) const {
+        mint res(*this);
+        return res+=a;
+    }
+    mint operator-(const mint& a) const {
+        mint res(*this);
+        return res-=a;
+    }
+    mint operator*(const mint& a) const {
+        mint res(*this);
+        return res*=a;
+    }
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+    // for prime mod
+    mint inv() const {
+        return pow(mod-2);
+    }
+    mint& operator/=(const mint& a) {
+        return (*this) *= a.inv();
+    }
+    mint operator/(const mint& a) const {
+        mint res(*this);
+        return res/=a;
+    }
+ 
+    friend ostream& operator<<(ostream& os, const mint& m){
+        os << (long long)m.x;
+        return os;
+    }
+};
+ 
+int main() {
+    long long a1, b1, c1;
+    cin >> a1 >> b1 >> c1;
+ 
+    mod = 1000000000;
+    mint b = b1;
+    mint t = b.pow(c1);
+    ll tt = t.x;
+ 
+    mint a = a1;
+    mint ans;
+    if (tt == 0)
+        ans = a.pow(mod);
+    else
+        ans = a.pow(tt);
+ 
+    prt((long long)ans.x % 10);
+ 
+    return 0;
+}
